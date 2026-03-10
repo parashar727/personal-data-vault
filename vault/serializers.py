dd04560323c87efe559ea1563b3e84fc1a959968
Aarush Parashar <aarush17062004@gmail.com>
2026-03-10 22:40:39 +0530

    Implement PATCH(partial update) fixes for VaultItems

--- vault/serializers.py
+++ vault/serializers.py
@@ -24,25 +24,26 @@ class VaultItemSerializer(serializers.ModelSerializer):
 
     def validate(self, attrs):
         request = self.context.get("request")
-        vault = attrs.get("vault")
-        item_type = attrs.get("item_type")
+        vault = attrs.get("vault") or getattr(self.instance, 'vault', None)
+        item_type = attrs.get("item_type") or getattr(self.instance, 'item_type', None)
         data = attrs.get("data")
 
-        if vault.owner != request.user:
+        if vault and vault.owner != request.user:
             raise serializers.ValidationError(
                 {"vault": "You can not make changes to another user's vault."}
             )
 
-        if item_type == "LOG":
-            if not data.get("email") or not data.get("password"):
-                raise serializers.ValidationError(
-                    {"data": "Email and Password fields are mandatory in Login items."}
-                )
+        if data is not None:
+            if item_type == "LOG":
+                if not data.get("email") or not data.get("password"):
+                    raise serializers.ValidationError(
+                        {"data": "Email and Password fields are mandatory in Login items."}
+                    )
 
-        elif item_type == "NOT":
-            if not data.get("content"):
-                raise serializers.ValidationError(
-                    {"data": "Content field is mandatory in Note type items."}
+            elif item_type == "NOT":
+                if not data.get("content"):
+                    raise serializers.ValidationError(
+                        {"data": "Content field is mandatory in Note type items."}
                 )
 
         return attrs
@@ -53,3 +54,11 @@ class VaultItemSerializer(serializers.ModelSerializer):
         validated_data["encrypted_data"] = json.dumps(data) #Implement encryption later, just plaintext JSON string for now
 
         return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        if "data" in validated_data:
+            data = validated_data.pop("data")
+
+            validated_data["encrypted_data"] = json.dumps(data)
+
+        return super().update(instance, validated_data)
